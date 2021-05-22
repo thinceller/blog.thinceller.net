188bf92f4964c4a423f4399ac23a05c9187951d0
thinceller <thinceller@gmail.com>
2021-05-23 02:43:30 +0900

    Remove nextra

--- next.config.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable */
-
-const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Index: NextPage = () => {
+  return (
+    <>
+      <h1>thinceller</h1>
+    </>
+  )
+}
+
+export default Index
