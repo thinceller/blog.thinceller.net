eb4c08953f68ba4dd22f50389ac129536c9abdc8
thinceller <thinceller@gmail.com>
2021-10-03 05:03:39 +0900

    Create About page

---
+++ pages/about.tsx
@@ -0,0 +1,54 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+import NextImage from 'next/image';
+import { chakra, Center, Heading, VStack, Text } from '@chakra-ui/react';
+import { Layout } from '../components/Layout';
+import { BLOG_NAME } from '../lib/constants';
+import AvatarImage from '../public/images/avatar.jpg';
+
+const ChakraNextImage = chakra(NextImage, {
+  shouldForwardProp: (prop) =>
+    [
+      'src',
+      'width',
+      'height',
+      'layout',
+      'loader',
+      'quality',
+      'priority',
+      'loading',
+      'lazyBoundary',
+      'placeholder',
+      'blurDataURL',
+      'unoptimized',
+      'alt',
+    ].includes(prop),
+});
+
+const About: NextPage = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>{`About | ${BLOG_NAME}`}</title>
+      </Head>
+      <VStack spacing={4} align="stretch">
+        <Heading as="h1" size="lg" sx={{ mt: 2 }}>
+          About
+        </Heading>
+        <Center>
+          <ChakraNextImage
+            src={AvatarImage}
+            alt="thinceller's avatar"
+            width="120px"
+            height="120px"
+            sx={{ borderRadius: '50%' }}
+          />
+        </Center>
+        <Text>thinceller / Kohei Kawakami</Text>
+        <Text>しがないソフトウェアエンジニアです。</Text>
+      </VStack>
+    </Layout>
+  );
+};
+
+export default About;
