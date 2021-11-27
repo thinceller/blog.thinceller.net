13fc003ec5738929be10cb73414e057f898a8bf5
thinceller <thinceller@gmail.com>
2021-11-28 00:09:05 +0900

    Fix date lib to use dayjs

--- lib/date.ts
+++ lib/date.ts
@@ -1,7 +1,16 @@
-import { format, parseISO } from 'date-fns';
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
 
+const tz = 'Asia/Tokyo';
+dayjs.extend(timezone);
+dayjs.tz.setDefault(tz);
+
+/**
+ * @param dateString ISO8601 date string
+ * @returns formatted date string (e.g. 2020-01-01)
+ */
 export const formatDate = (dateString: string): string => {
-  const date = parseISO(dateString);
+  const date = dayjs(dateString);
 
-  return format(date, 'yyyy-MM-dd');
+  return date.format('YYYY-MM-DD');
 };
