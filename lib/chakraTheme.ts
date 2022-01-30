b4eb5b70328adb634a8926b56c81b258edca79c8
thinceller <thinceller@gmail.com>
2022-01-30 23:13:58 +0900

    style: add margin and padding

--- lib/chakraTheme.ts
+++ lib/chakraTheme.ts
@@ -5,6 +5,10 @@ const theme = extendTheme({
     heading: 'Noto Sans JP, system-ui, sans-serif',
     body: 'Noto Sans JP, system-ui, sans-serif',
   },
+  lineHeights: {
+    base: 1.8,
+    tall: 1.9,
+  },
 });
 
 export { theme };
