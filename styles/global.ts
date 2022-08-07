62d258f91b35c0506282ac772008637f141e3e76
thinceller <thinceller@gmail.com>
2022-08-07 19:08:13 +0900

    chore: remove focus-visible polyfill

--- styles/global.ts
+++ styles/global.ts
@@ -11,15 +11,6 @@ export const globalStyles = css`
   }
 
   /**
-   * This will hide the focus indicator if the element receives focus    via the mouse,
-   * but it will still show up on keyboard focus.
-   */
-  .js-focus-visible :focus:not([data-focus-visible-added]) {
-    outline: none;
-    box-shadow: none;
-  }
-
-  /**
    * Headings anchor link
    */
   .header-anchor-link {
