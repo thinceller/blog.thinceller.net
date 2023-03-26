d262c9030831e892cea5a1c0cabae18dbb835824
thinceller <thinceller@gmail.com>
2023-03-26 19:04:41 +0900

    enable textlint-plugin-markdown

--- .textlintrc.js
+++ .textlintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
   filters: {},
+  plugins: {
+    '@textlint/markdown': {
+      extensions: ['.mdx'],
+    },
+  },
   rules: {
     'preset-ja-technical-writing': {
       'sentence-length': {
         max: 200,
-      },
-      'ja-no-mixed-period': {
-        allowPeriodMarks: [':::'],
       },
     },
     'preset-ja-spacing': true,
