b7d521ca443ca2b0eda615a1292fedd36fb7c630
thinceller <thinceller@gmail.com>
2023-05-14 17:01:17 +0900

    use next/font

--- lib/chakraTheme.ts
+++ lib/chakraTheme.ts
@@ -1,9 +1,16 @@
 import { extendTheme } from '@chakra-ui/react';
+import { Noto_Sans_JP } from 'next/font/google';
+
+const notoSansJp = Noto_Sans_JP({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  fallback: ['system-ui', 'sans-serif'],
+});
 
 const theme = extendTheme({
   fonts: {
-    heading: 'Noto Sans JP, system-ui, sans-serif',
-    body: 'Noto Sans JP, system-ui, sans-serif',
+    heading: notoSansJp.style.fontFamily,
+    body: notoSansJp.style.fontFamily,
   },
   lineHeights: {
     base: 1.8,
